0478e994648ac15aa0a42f9898151189fd06dadc
Samir Katakamsetty <katakamsettysamir@gmail.com>
2026-05-15 16:57:11 -0400

    Daily Return now works, deduplicated the staging data.

--- backend/analytics/models/staging/stg_stocks.sql
+++ backend/analytics/models/staging/stg_stocks.sql
@@ -13,6 +13,21 @@ renamed AS (
     FROM source
     WHERE "Ticker" IS NOT NULL
         AND "Close" IS NOT NULL
+),
+deduplicated AS (
+    SELECT 
+        *,
+        ROW_NUMBER() OVER(PARTITION BY ticker, price_date ORDER BY volume DESC) as rn
+    FROM renamed
 )
 
-SELECT * FROM renamed
+SELECT 
+    price_date,
+    ticker,
+    open_price,
+    high_price,
+    low_price,
+    close_price,
+    volume
+FROM deduplicated
+WHERE rn = 1
